9d0b18647cef98dcc8dfbf98a175b39110f9ca0d
Jan <admin@wkzn.ru>
2018-10-09 15:25:59 +0300

    end add php

--- assay.php
+++ assay.php
@@ -6,3 +6,4 @@ if(isset($_POST['addend1'])) $addend1 = $_POST['addend1'];
 if (isset($_POST['addend2'])) $addend2 = $_POST['addend2']; 
 echo "Первое слагаемое: $addend1  <br> Второе слагаемое: $addend2";
 }
+?>
