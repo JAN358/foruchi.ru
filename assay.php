5140b6fff72c4f74687466e3db3819185e2904d2
Jan <admin@wkzn.ru>
2018-10-09 15:37:19 +0300

    remove end

--- assay.php
+++ assay.php
@@ -6,4 +6,3 @@ if(isset($_GET['addend1'])) $addend1 = $_GET['addend1'];
 if (isset($_GET['addend2'])) $addend2 = $_GET['addend2']; 
 echo "Первое слагаемое: $addend1  <br> Второе слагаемое: $addend2";
 }
-?>
