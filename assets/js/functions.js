32a56852f104ed268e649c9734a323cdfcceb519
Jan <admin@wkzn.ru>
2018-10-09 15:47:22 +0300

    new

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,6 +4,22 @@ function getRandomFloat(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+        url:     url, //url страницы (action_ajax_form.php)
+        type:     "GET", //метод отправки
+        dataType: "html", //формат данных
+        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        success: function(response) { //Данные отправлены успешно
+        	result = $.parseJSON(response);
+        	$('.out').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
+    	},
+    	error: function(response) { // Данные не отправлены
+            $('.out').html('Ошибка. Данные не отправлены.');
+    	}
+ 	});
+}
+
   $(document).ready(function () {
 
 	var a, b, c;
@@ -17,15 +33,10 @@ function getRandomFloat(min, max) {
 
 	$("#form").on( "submit", function( event ) {
 	  event.preventDefault();
-	  	$.ajax({
-	    type: "GET",
-	    url: "assay.php",
-	    addend1: a,
-	    addend2: b,
-	    success: function(data) {
-	          $('.out').text(data);
-	       }
-	    });   
+	  function(){
+			sendAjaxForm('result_form', 'ajax_form', 'assay.php');
+			return false; 
+		}  
 	});
 
   });
