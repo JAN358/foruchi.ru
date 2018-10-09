99ef4409e6ed5cafda9cd39f2da856a8be942e39
Jan <admin@wkzn.ru>
2018-10-09 15:36:05 +0300

    .serialize()

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -20,8 +20,8 @@ function getRandomFloat(min, max) {
 	  	$.ajax({
 	    type: "GET",
 	    url: "assay.php",
-	    addend1: a,
-	    addend2: b,
+	    addend1: a.serialize(),
+	    addend2: b.serialize(),
 	    success: function(data) {
 	          $('.out').text(data);
 	       }
