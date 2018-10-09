04d92c604af7086bbbeb04a60a44a5e10d84bc05
Jan <admin@wkzn.ru>
2018-10-09 16:03:06 +0300

    3

--- assay.php
+++ assay.php
@@ -1,13 +1,4 @@
 <?php
-if((isset($_GET['addend1'])&&$_GET['addend1']!="")&&(isset($_GET['addend2'])&&$_GET['addend2']!="")){
-$addend1 = "Не известно";
-$addend2 = "Не известно";
-if(isset($_GET['addend1'])) $addend1 = $_GET['addend1'];
-if (isset($_GET['addend2'])) $addend2 = $_GET['addend2']; 
-echo "Первое слагаемое: $addend1  <br> Второе слагаемое: $addend2";
-}
-
-<?php
 
 if (isset($_POST["addend1"]) && isset($_POST["addend2"]) ) { 
 
