d85c0cf08b4bdec6962101513eb9e367ef3e98b9
Jan <admin@wkzn.ru>
2018-10-09 14:51:34 +0300

    data2

--- assay.php
+++ assay.php
@@ -5,6 +5,6 @@ $addend2 = "Не известно";
 if(isset($_POST['addend1'])) $addend1 = $_POST['addend1'];
 if (isset($_POST['addend2'])) $addend2 = $_POST['addend2']; 
 echo "Первое слагаемое: $addend1  <br> Второе слагаемое: $addend2";
-}
+};
 
 ?>
